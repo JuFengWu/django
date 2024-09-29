daa53364342435c50ae7000a0313f92cd01058ac
leowu <leowu102000@gmail.com>
2024-09-29 21:43:05 +0800

    use one page to show up

--- stock/web_tool/views.py
+++ stock/web_tool/views.py
@@ -35,51 +35,46 @@ def stock_chart(request):
 
 
 def test_view(request):
-    if request.method == 'POST':
-        # 獲取按鈕動作，通過 GET 參數傳遞
-        action = request.GET.get('action')
-
-        # 獲取選中的股票代碼列表
+    if request.method == "POST":
+        print("post")
+        # 假設接收到的股票代碼和日期範圍
         selected_stocks = request.POST.getlist('selected_stocks')
-
-        # 獲取選中的股票代碼列表
         selected_stocks2 = request.POST.getlist('selected_stocks2')
-
-        # 獲取開始和結束日期
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
-
-        # 獲取 window size 的值
         window_size = request.POST.get('window_size')
+        print(selected_stocks)
+        print(selected_stocks2)
+        
+        data = spider_data(selected_stocks[0],start_date,end_date)
+        data2= spider_data(selected_stocks2[0],start_date,end_date)
 
-        # 根據不同按鈕進行不同的邏輯處理
-        if action == 'submit1':
-            response_message = (
-                f"按下的是按鈕 1\n"
-                f"股票代碼: {', '.join(selected_stocks)}\n"
-                f"開始日期: {start_date}\n"
-                f"結束日期: {end_date}\n"
-                f"窗口大小: {window_size}"
-            )
-        else:
-            response_message = "未知的操作"
+        log1, log2, spread, moving_avg_result, moving_std_result, upperline, downline = strategy(data,data2)
+    
+        buy1Time,buy2Time,sell1Time,sell2Time = buy_and_sell(spread, moving_avg_result, upperline, downline)
 
-        # 假設用戶只選擇了一個股票代碼，我們取第一個
-        stock_symbol = selected_stocks[0] if selected_stocks else 'AAPL'
-        stock_symbol2 = selected_stocks2[0] if selected_stocks2 else 'AAPL'
+        
+        show_upperline = []
+        show_downline = []
+        show_moving_avg_result=[]
+        for i in range(len(data)):
+            timestamp = data[i][0]
+            show_upperline.append([timestamp, upperline[i]])
+            show_downline.append([timestamp, downline[i]])
+            show_moving_avg_result.append([timestamp, moving_avg_result[i]])
 
-        print("stock_symbol in slect is ",stock_symbol)
-
-        # 返回帶有 Highcharts 圖表的 HTML，並傳遞股票代碼和其他參數到模板
-        context = {
-            'stock_symbol': stock_symbol,
-            "stock_symbol2" : stock_symbol2,
-            'start_date': start_date,
-            'end_date': end_date,
-            'window_size': window_size,
+        data = {
+            'stock_data': data,  # 假設數據
+            'stock_data2': data2,
+            'spread': spread,
+            'upperline': show_upperline,
+            'downline': show_downline,
+            'averageLine': show_moving_avg_result,
+            'buy1time': "2024-09-01 10:00:00",
+            'buy2time': "2024-09-01 11:00:00",
+            'sell1Time': "2024-09-01 12:00:00",
+            'sell2Time': "2024-09-01 13:00:00"
         }
-        return render(request, 'stock_chart.html', context)
+        return JsonResponse(data)
+    return render(request, 'stock_chart.html')
 
-        return HttpResponse(f"<pre>{response_message}</pre>")
-
-    return render(request, 'stock_chat_input.html')
