5966d70118125cd25afe6227aab96d195a541822
leowu <leowu102000@gmail.com>
2024-10-17 08:45:22 +0800

    finish HW34

--- bio/web_tool/views_hw34.py
+++ bio/web_tool/views_hw34.py
@@ -9,17 +9,22 @@ def transcript(request, gene_sequence_name):
     result = get_filtered_data(bedgraph,gene_sequence_name)
     barData = expand_position_value(result)
 
+    bedgraph = './web_tool/data/SRR20334757_m1_bedgraph.csv'
+    result = get_filtered_data(bedgraph,gene_sequence_name)
+    barData2 = expand_position_value(result)
+
+    bedgraph = './web_tool/data/SRR20334757_m2_bedgraph.csv'
+    result = get_filtered_data(bedgraph,gene_sequence_name)
+    barData3 = expand_position_value(result)
+
     file_path = './web_tool/data/spliced_codingtranscript_293.csv'
     exon_list, other_list = get_exon_and_other(gene_sequence_name,file_path)
     new_exon_list=convert_to_dict_list(exon_list)
+    new_other_list=convert_to_dict_list(other_list)
+    print(new_other_list)
 
-    barHorizontal = [
-        {"type": "UTR", "start": 0, "end": 300, "color": "gray"},
-        {"type": "CDS", "start": 300, "end": 1000, "color": "green"},
-        {"type": "EXON", "start": 100, "end": 400, "color": "orange"},
-        {"type": "EXON", "start": 500, "end": 800, "color": "yellow"}
-    ]
-    context = {'barData': barData, 'barHorizontal':new_exon_list ,
+    context = {'barData': barData, 'barData2': barData2, 'barData3': barData3, 
+               'barHorizontal':new_exon_list ,"other_list":new_other_list,
                "gene_sequence_name" : gene_sequence_name}
     
     return render(request, 'transcript.html', context)
