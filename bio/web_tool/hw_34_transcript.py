fac35f2e7b180ed8c3b742e99f5ac8f3a7a59ce3
leowu <leowu102000@gmail.com>
2024-10-17 08:29:09 +0800

    ready to expend

--- bio/web_tool/hw_34_transcript.py
+++ bio/web_tool/hw_34_transcript.py
@@ -9,16 +9,42 @@ def get_exon_and_other(name, file_path):
     filtered_data = dataframe[dataframe['name'] == name]
     
     for _, row in filtered_data.iterrows():
-        start_end_tuple = (int(row['start']), int(row['end']))
+        start_end_tuple = (int(row['start']), int(row['end']), row['type'])
         if 'exon' in row['type']:
             exon_list.append(start_end_tuple)
         else:
             other_list.append(start_end_tuple)
     
-    combined = exon_list + other_list
-    max_value = max([max(pair) for pair in combined])
+    # 給 exon_list 奇數加 yellow，偶數加 orange
+    for i, exon in enumerate(exon_list):
+        if i % 2 == 0:  # 偶數索引
+            exon_list[i] = exon + ('orange',)
+        else:  # 奇數索引
+            exon_list[i] = exon + ('yellow',)
     
-    return exon_list, other_list,max_value
+    # 給 other_list 奇數加 green，偶數加 gray
+    for i, other in enumerate(other_list):
+        if i % 2 == 0:  # 偶數索引
+            other_list[i] = other + ('gray',)
+        else:  # 奇數索引
+            other_list[i] = other + ('green',)
+    
+    return exon_list, other_list
+
+def convert_to_dict_list(input_list):
+    output_list = []
+    
+    for item in input_list:
+        # 將元組轉換為字典
+        dict_item = {
+            "type": item[2],
+            "start": item[0],
+            "end": item[1],
+            "color": item[3]
+        }
+        output_list.append(dict_item)
+    
+    return output_list
 
 def get_filtered_data(file_path, ref_id):
     # Load the CSV file
@@ -43,12 +69,15 @@ def expand_position_value(data_list):
 if __name__ == "__main__":
     file_path = './web_tool/data/spliced_codingtranscript_293.csv'  # Replace with the actual file path
     target_name = '2L52.1a.1'
-    exon_list, other_list, max_value = get_exon_and_other(target_name,file_path)
+    exon_list, other_list = get_exon_and_other(target_name,file_path)
     print(exon_list)
     print(other_list)
+    new_exon_list=convert_to_dict_list(exon_list)
+    print(new_exon_list)
+    """
     bedgraph = './web_tool/data/SRR20334757_m0_bedgraph.csv'
     result = get_filtered_data(bedgraph,target_name)
     print(result)
     data = expand_position_value(result)
     print(data)
-    
+    """
--- bio/web_tool/views_hw34.py
+++ bio/web_tool/views_hw34.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from web_tool.request_data import *
-from web_tool.hw_34_transcript import get_exon_and_other,get_filtered_data,expand_position_value
+from web_tool.hw_34_transcript import get_exon_and_other,convert_to_dict_list,get_filtered_data,expand_position_value
 
 def transcript(request, gene_sequence_name):
 
@@ -9,18 +9,9 @@ def transcript(request, gene_sequence_name):
     result = get_filtered_data(bedgraph,gene_sequence_name)
     barData = expand_position_value(result)
 
-    #print(barData)
-   
-    #barData = [
-    #     {'position': 1666, 'value': 1.0}, {'position': 1667, 'value': 1.0}
-    #]
-    data = [
-        
-        {'start': 200, 'end': 308, 'read_count': 108, 'values': 16.0},
-        {'start': 400, 'end': 450, 'read_count': 50, 'values': 8.0},
-        {'start': 1630, 'end': 1638, 'read_count': 32,  'values': 1.0},
-        # 可以加入更多數據對
-    ]
+    file_path = './web_tool/data/spliced_codingtranscript_293.csv'
+    exon_list, other_list = get_exon_and_other(gene_sequence_name,file_path)
+    new_exon_list=convert_to_dict_list(exon_list)
 
     barHorizontal = [
         {"type": "UTR", "start": 0, "end": 300, "color": "gray"},
@@ -28,8 +19,8 @@ def transcript(request, gene_sequence_name):
         {"type": "EXON", "start": 100, "end": 400, "color": "orange"},
         {"type": "EXON", "start": 500, "end": 800, "color": "yellow"}
     ]
-    context = {'barData': barData, 'barHorizontal':barHorizontal ,
-               "gene_sequence_name" : gene_sequence_name,'data':data}
+    context = {'barData': barData, 'barHorizontal':new_exon_list ,
+               "gene_sequence_name" : gene_sequence_name}
     
     return render(request, 'transcript.html', context)
 
