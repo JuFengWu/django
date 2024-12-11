de79a34a6550cf623122a5003b999bc966aea767
leowu <leowu102000@gmail.com>
2024-12-11 09:20:31 +0800

    expand to 4 flow

--- stock/web_tool/hw_eps.py
+++ stock/web_tool/hw_eps.py
@@ -9,14 +9,43 @@ def eps_show(request):
 def pe_flow_data(request):
     # 模擬的數據範例
     data = {
-        "ranges": [
-            {"label": "昂貴價區間", "start": 90, "end": 130, "color": "red"},
-            {"label": "合理到昂貴價區間", "start": 70, "end": 90, "color": "lightcoral"},
-            {"label": "便宜到合理價區間", "start": 40, "end": 70, "color": "lightgreen"},
-            {"label": "便宜價區間", "start": 0, "end": 40, "color": "yellow"}
-        ],
-        "latest_price": 85,  # 最新價格
+        "latest_price": 85,
+        "charts": [
+            {
+                "ranges": [
+                    {"label": "昂貴價區間", "start": 90, "end": 130, "color": "red"},
+                    {"label": "合理到昂貴價區間", "start": 70, "end": 90, "color": "lightcoral"},
+                    {"label": "便宜到合理價區間", "start": 40, "end": 70, "color": "lightgreen"},
+                    {"label": "便宜價區間", "start": 0, "end": 40, "color": "yellow"}
+                ]
+            },
+            {
+                "ranges": [
+                    {"label": "昂貴價區間", "start": 100, "end": 150, "color": "red"},
+                    {"label": "合理到昂貴價區間", "start": 80, "end": 100, "color": "lightcoral"},
+                    {"label": "便宜到合理價區間", "start": 50, "end": 80, "color": "lightgreen"},
+                    {"label": "便宜價區間", "start": 0, "end": 50, "color": "yellow"}
+                ]
+            },
+            {
+                "ranges": [
+                    {"label": "昂貴價區間", "start": 110, "end": 140, "color": "red"},
+                    {"label": "合理到昂貴價區間", "start": 70, "end": 110, "color": "lightcoral"},
+                    {"label": "便宜到合理價區間", "start": 30, "end": 70, "color": "lightgreen"},
+                    {"label": "便宜價區間", "start": 0, "end": 30, "color": "yellow"}
+                ]
+            },
+            {
+                "ranges": [
+                    {"label": "昂貴價區間", "start": 120, "end": 160, "color": "red"},
+                    {"label": "合理到昂貴價區間", "start": 100, "end": 120, "color": "lightcoral"},
+                    {"label": "便宜到合理價區間", "start": 60, "end": 100, "color": "lightgreen"},
+                    {"label": "便宜價區間", "start": 0, "end": 60, "color": "yellow"}
+                ]
+            }
+        ]
     }
+
     return JsonResponse(data)
 
 from django.http import JsonResponse
